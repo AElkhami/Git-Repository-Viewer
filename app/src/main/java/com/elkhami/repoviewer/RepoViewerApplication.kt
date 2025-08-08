7492e5d77b51037c07cd6d8ec4ab3daee7e1887a
Ahmed Elkhami <ahmedeelkhami@gmail.com>
2025-08-08 09:58:57 +0200

    Feature/bug fixes (#13)
    * Bug fixes: Bug fixes and enhancements.
    
    * Bug fixes: Bug fixes and enhancements.
    
    * Adding some UI tests.
    
    * Adding some UI tests.
    
    * Clean up tracked IDE files
    
    * Adding some fixes and enhancements.

--- app/src/main/java/com/elkhami/repoviewer/RepoViewerApplication.kt
+++ app/src/main/java/com/elkhami/repoviewer/RepoViewerApplication.kt
@@ -3,6 +3,7 @@ package com.elkhami.repoviewer
 import android.app.Application
 import com.elkhami.core.data.di.coreDataModule
 import com.elkhami.core.database.di.databaseModule
+import com.elkhami.core.presentation.di.uiComponentsModule
 import com.elkhami.repoviewer.data.di.repoViewerDataModule
 import com.elkhami.repoviewer.presentation.di.repoViewerViewModelModule
 import timber.log.Timber
@@ -22,6 +23,7 @@ class RepoViewerApplication: Application() {
             androidLogger()
             androidContext(this@RepoViewerApplication)
             modules(
+                uiComponentsModule,
                 coreDataModule,
                 repoViewerViewModelModule,
                 repoViewerDataModule,
--- repo-viewer/presentation/src/main/java/com/elkhami/repoviewer/presentation/di/RepoViewerModule.kt
+++ repo-viewer/presentation/src/main/java/com/elkhami/repoviewer/presentation/di/RepoViewerModule.kt
@@ -1,7 +1,7 @@
 package com.elkhami.repoviewer.presentation.di
 
 import com.elkhami.repoviewer.presentation.repolist.RepoListViewModel
-import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val repoViewerViewModelModule = module {
